cfceb8f847acad476339b34d587b30c2397658ba
VuDej <drvu47@gmail.com>
2022-06-18 06:51:50 +0200

    added component "single reservation" and functionality for counting days and total cost of rental

---
+++ src/pages/reservations/SingleReservation.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteReservationFromApi } from '../../redux/reducers/reservations/reservations';
+import DaysOfRental from './Count';
+
+const Reservation = (props) => {
+  const { reservation } = props;
+  const dispatch = useDispatch();
+  const cars = useSelector((state) => state.reducerCars);
+
+  const totalPriceOfRental = (id) => {
+    let total = 0;
+    cars.forEach((car) => {
+      if (car.id === id) {
+        total += DaysOfRental(reservation.pick_up_day, reservation.return_day) * car.price_per_day;
+      }
+    });
+    return total;
+  };
+
+  const getCarName = (id) => {
+    let carName = '';
+    cars.forEach((car) => {
+      if (car.id === id) {
+        carName = car.carName;
+      }
+    });
+    return carName;
+  };
+
+  const [name, setName] = useState('');
+  useEffect(() => {
+    Axios.get('http://localhost:3001/current_user', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
+      },
+    }).then((response) => {
+      setName(response.data.name);
+    });
+  }, []);
+
+  return (
+    <section className="reservation">
+      <table id="cars">
+        <tr>
+          <th>Car</th>
+          <th>User</th>
+          <th>Pick Up Day</th>
+          <th>Return Day</th>
+          <th>Pick up City</th>
+          <th>Return City</th>
+          <th>Total Days</th>
+          <th>Total Price</th>
+        </tr>
+        <tr>
+          <td>{getCarName(reservation.car_id)}</td>
+          <td>{name}</td>
+          <td>{reservation.pick_up_day}</td>
+          <td>{reservation.return_day}</td>
+          <td>{reservation.pick_up_city}</td>
+          <td>{reservation.return_city}</td>
+          <td>
+            {DaysOfRental(reservation.pick_up_day, reservation.return_day)}
+          </td>
+          <td>
+            $
+            {
+            totalPriceOfRental(reservation.car_id)
+            }
+          </td>
+        </tr>
+      </table>
+
+      <div className="car-reserve-btn">
+        <button
+          type="button"
+          className="btn btn-outline-danger reservation-button"
+          onClick={() => dispatch(deleteReservationFromApi(reservation.id))}
+        >
+          Cancel
+        </button>
+      </div>
+    </section>
+  );
+};
+
+Reservation.propTypes = {
+  reservation: PropTypes.instanceOf(Object).isRequired,
+};
+export default Reservation;
