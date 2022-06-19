066a1d6728263737a07694b7538b88d72a46daf2
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-19 23:22:43 +0200

    Add some logic to display the car imahe

--- src/pages/CarDetails/CarDetails.js
+++ src/pages/CarDetails/CarDetails.js
@@ -1,9 +1,9 @@
 // import React, { useState, useEffect } from 'react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { RiArrowLeftSFill } from 'react-icons/ri';
 import { getCarsFromAPI } from '../../redux/reducers/cars';
-// import { useParams, NavLink } from 'react-router-dom';
 import style from './Detail.module.css';
 
 const CarDetails = () => {
@@ -25,10 +25,32 @@ const CarDetails = () => {
     }
   });
 
+  if (displayCar) {
+    return (
+      <section className={style.show_container}>
+        <div className={style.show_img}>
+          <div className={style.img_div}>
+            <img alt="house" src={displayCar.carImg} />
+          </div>
+          <Link
+            to={{
+              pathname: '/home',
+            }}
+          >
+            <button type="button" className={style.home}>
+              left
+              <RiArrowLeftSFill color="danger" sx={{ fontSize: 50 }} />
+            </button>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
   return (
-    <section className={style.car}>
+    <div className={style.car}>
       <h1>Loading...</h1>
-    </section>
+    </div>
   );
 };
 
