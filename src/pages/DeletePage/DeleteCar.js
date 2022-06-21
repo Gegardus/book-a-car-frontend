55e1fb5bb2900bc1cae050548d7be7d9bcd94763
verissimo francis <verissimotoyin50@gmail.com>
2022-06-21 09:22:38 +0100

    Update delete page

--- src/pages/DeletePage/DeleteCar.js
+++ src/pages/DeletePage/DeleteCar.js
@@ -1,24 +1,17 @@
-import { React, useEffect } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Sidebar from '../../components/navigation/Sidebar';
 import Hamburger from '../../components/navigation/Hamburger';
-import { getCarsFromAPI } from '../../redux/reducers/cars';
-import { deleteCarFromAPI } from '../../redux/reducers/deleteCars';
+import { removeCarFromAPI } from '../../redux/reducers/cars';
 import './delete.css';
 
 const DeleteCar = () => {
-  const fetchedCars = useSelector((state) => state.cars);
-  const cars = fetchedCars.data;
+  const cars = useSelector((state) => state.cars);
   const dispatch = useDispatch();
-
   const handleDelete = (id) => {
-    dispatch(deleteCarFromAPI(id));
+    dispatch(removeCarFromAPI(id));
     window.location.reload();
   };
-
-  useEffect(() => {
-    dispatch(getCarsFromAPI());
-  }, []);
 
   return (
     <section className="Delete-page">
@@ -27,14 +20,13 @@ const DeleteCar = () => {
         <Sidebar />
       </div>
       <div>
-        <h1 className="header">Delete Items</h1>
+        <div className="header">
+          <h1>Delete Items</h1>
+        </div>
         {cars.map((car) => (
           <div key={car.id}>
-            <img
-              src={car.image}
-              alt={`${car.make} ${car.model}`}
-            />
-            <div>{car.model}</div>
+            <img src={car.carImg} alt="delete" />
+            <div>{car.carModel}</div>
             <button onClick={() => handleDelete(car.id)} type="button">
               Delete
             </button>
